f0a8cbf5e62160f860c5dc3657f834dddf2a8ff5
GopalAsu <91899516+VarrelKun@users.noreply.github.com>
2022-08-26 00:42:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -61,7 +61,7 @@ global.psaweria = 'https://discord.io/FuzePage'
 /*============== NOMOR ==============*/
 global.nomorbot = '6287739946665'
 global.nomorown = '6285954184111'
-global.namebot = 'FUZEBOT'
+global.namebot = '𝐏𝐄𝐍𝐓𝐀𝐆𝐑𝐀𝐌'
 global.nameown = 'SIJEMBOT'
 
 
@@ -125,9 +125,9 @@ global.author = global.wm
 
 
 /*============== LOGO ==============*/
-global.thumb = 'https://telegra.ph/file/a791e0530456d0f5bcb8e.png' //Main Thumbnail
-global.thumb2 = 'https://telegra.ph/file/a791e0530456d0f5bcb8e.png'
-global.thumbbc = 'https://telegra.ph/file/a791e0530456d0f5bcb8e.png' //For broadcast
+global.thumb = 'https://telegra.ph/file/162d60896b53c2e885c40.jpg' //Main Thumbnail
+global.thumb2 = 'https://telegra.ph/file/162d60896b53c2e885c40.jpg'
+global.thumbbc = 'https://telegra.ph/file/162d60896b53c2e885c40.jpg' //For broadcast
 global.giflogo = 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4'
 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
@@ -143,7 +143,7 @@ global.dxlsx = 'application/vnd.openxmlformats-officedocument.spreadsheetml.shee
 global.dpdf = 'application/pdf'
 global.drtf = 'text/rtf'
 
-global.thumbdoc = 'https://telegra.ph/file/a791e0530456d0f5bcb8e.png'
+global.thumbdoc = 'https://telegra.ph/file/162d60896b53c2e885c40.jpg'
 
 /*=========== FAKE SIZE ===========*/
 global.fsizedoc = '99999999999999' // default 10TB
