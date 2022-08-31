68f2523e1a34d5f10048712015393b2d35a2b167
GopalAsu <91899516+VarrelKun@users.noreply.github.com>
2022-08-31 07:51:27 +0700

    Update nsfw-nhentai.js

--- plugins/nsfw-nhentai.js
+++ plugins/nsfw-nhentai.js
@@ -5,12 +5,13 @@ let handler = async (m, { conn, args }) => {
 	let code = (args[0] || '').replace(/\D/g, '')
 	if (!code) throw 'Input code' 
 	await m.reply('_In progress, please wait..._')
-	let res = await fetch('https://expressjs-akkun.up.railway.app/nhentai?code=' + code)
+    let res = 'https://api.lolhuman.xyz/api/nhentaipdf/'
+    let api = '?apikey=e54205a4ca2caa368cc067bb'
 	if (!res.ok) throw await res.statusText
 	let json = await res.json()
 	let buffer = await (await fetch('https://external-content.duckduckgo.com/iu/?u=' + json.result.pages[0])).buffer()
 	let jpegThumbnail = await extractImageThumb(buffer)
-	conn.sendMessage(m.chat, { document: { url: 'https://expressjs-akkun.up.railway.app/nhentai/' + code }, jpegThumbnail, fileName: json.result.title + '.pdf', mimetype: 'application/pdf' }, { quoted: m })
+	conn.sendMessage(m.chat, { document: { res + code + api }, jpegThumbnail, fileName: json.result.title + '.pdf', mimetype: 'application/pdf' }, { quoted: m })
 }
 handler.help = ['nhentai']
 handler.tags = ['nsfw']
@@ -19,3 +20,5 @@ handler.command = /^(nhentai)$/i
 handler.limit = true
 
 export default handler
+
+//https://api.lolhuman.xyz/api/nhentaipdf/344253?apikey=a70fe5fb01ea65b6bb350009
