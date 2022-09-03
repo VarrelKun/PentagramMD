be3975074bd3b00e284bc98c0dfba57642882965
GopalAsu <91899516+VarrelKun@users.noreply.github.com>
2022-09-03 16:36:30 +0700

    Update cecanlist.js

--- plugins/cecanlist.js
+++ plugins/cecanlist.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import fetch from 'node-fetch'
 let handler = async (m, {command, conn}) => {
 	
 	let tekk = `\`\`\`➩ Ini Istri Owner >_< \`\`\` `
@@ -8,23 +9,23 @@ let haha = await conn.getFile(`https://api-reysekha.herokuapp.com/api/wallpaper/
 conn.sendButton(m.chat, tekk.trim(), author, haha.data, [[' Next', `/${command}`]], m)}
 	
 if (command == 'ind') {
-let haha = await conn.getFile(`https://api.zeeoneofc.xyz/api/cecan/indonesia?apikey=8oeBfLv2`)
+let haha = await conn.getFile(`https://api.zeeoneofc.xyz/api/cecan/indonesia?apikey=Rs9ma8MZ`)
 conn.sendButton(m.chat, tekk.trim(), author, haha.data, [[' Next', `/${command}`]], m)}
 
 if (command == 'jap') {
-let haha = await conn.getFile(`https://api.zeeoneofc.xyz/api/cecan/japan?apikey=8oeBfLv2`)
+let haha = await conn.getFile(`https://api.zeeoneofc.xyz/api/cecan/japan?apikey=Rs9ma8MZ`)
 conn.sendButton(m.chat, tekk.trim(), author, haha.data, [[' Next', `/${command}`]], m)}
 
 if (command == 'chn') {
-let haha = await conn.getFile(`https://api.zeeoneofc.xyz/api/cecan/china?apikey=8oeBfLv2`)
+let haha = await conn.getFile(`https://api.zeeoneofc.xyz/api/cecan/china?apikey=Rs9ma8MZ`)
 conn.sendButton(m.chat, tekk.trim(), author, haha.data, [[' Next', `/${command}`]], m)}
 
 if (command == 'mly') {
-let haha = await conn.getFile(`https://api.zeeoneofc.xyz/api/cecan/malaysia?apikey=8oeBfLv2`)
+let haha = await conn.getFile(`https://api.zeeoneofc.xyz/api/cecan/malaysia?apikey=Rs9ma8MZ`)
 conn.sendButton(m.chat, tekk.trim(), author, haha.data, [[' Next', `/${command}`]], m)}
 
 if (command == 'kra') {
-let haha = await conn.getFile(`https://api.zeeoneofc.xyz/api/cecan/korea?apikey=8oeBfLv2`)
+let haha = await conn.getFile(`https://api.zeeoneofc.xyz/api/cecan/korea?apikey=Rs9ma8MZ`)
 conn.sendButton(m.chat, tekk.trim(), author, haha.data, [[' Next', `/${command}`]], m)}
 
 
