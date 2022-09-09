58beb65c25f7de18141aa45c2c40cabe5c7f7a4b
GopalAsu <91899516+VarrelKun@users.noreply.github.com>
2022-09-10 00:21:26 +0700

    Update group-link.js

--- plugins/group-link.js
+++ plugins/group-link.js
@@ -1,19 +1,28 @@
-import { areJidsSameUser } from '@adiwajshing/baileys'
-let handler = async (m, { conn, args }) => {
-    let group = m.chat
-    if (/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(args[0])) group = args[0]
-    if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw 'Hanya bisa dibuka di grup'
-    let groupMetadata = await conn.groupMetadata(group)
-    if (!groupMetadata) throw 'groupMetadata is undefined :\\'
-    if (!('participants' in groupMetadata)) throw 'participants is not defined :('
-    let me = groupMetadata.participants.find(user => areJidsSameUser(user.id, conn.user.id))
-    if (!me) throw 'Aku tidak ada di grup itu :('
-    if (!me.admin) throw 'Aku bukan admin T_T'
-    m.reply('https://chat.whatsapp.com/' + await conn.groupInviteCode(group))
+let handler = async (m, { conn }) => {
+  try {
+    /*conn.sendTemplateButtonDoc(m.chat, col, `*Link Group:* ${await conn.getName(m.chat)}\n\nhttps://chat.whatsapp.com/` + await conn.groupInviteCode(m.chat), wm, 'Info Grup', '.infogrup', m)*/
+
+conn.sendMessage(m.chat, {
+  text: `*Link Group:* ${await conn.getName(m.chat)}`,
+  templateButtons: [{
+    index: 1,
+    urlButton: {
+      displayText: `Click`,
+      url: 'https://www.whatsapp.com/otp/copy/https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat)
+    }
+  }],
+  footer: wm
+})
+
+  } catch {
+    conn.reply(m.chat, `Jadikan @${conn.user.jid.split('@')[0]} sebagai admin untuk menggunakan perintah ini!`, m, { mentions: [conn.user.jid] })
+  }
 }
 handler.help = ['linkgroup']
 handler.tags = ['group']
-handler.command = /^link(gro?up)?$/i
+handler.command = /^link(g(c)?ro?up)?$/i
 
+handler.group = true
+//handler.botAdmin = true
 
-export default handler
+export default handler 
