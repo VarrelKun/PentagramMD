a82b7a34c4104438bb5d6743099d4d2ea7a2c5ac
BlackGopal <77979054+BlackGopal@users.noreply.github.com>
2022-09-21 03:36:58 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,97 +1,48 @@
-let { MessageType } = (await import('@adiwajshing/baileys')).default
 
-let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
-  let type = (args[0] || '').toLowerCase()
-  let _type = (args[0] || '').toLowerCase()
+import fetch from 'node-fetch'
+import axios from 'axios'
+import fs from 'fs'
 
-//------- NOMOR
-  let nowner = `${nomorown.split`@`[0]}@s.whatsapp.net`
-  let teksnomor = `${htki} *OWNER* ${htka}
-• @${nomorown.split`@`[0]} •
-------- ${nameown} -------
+let handler = async (m, { conn, args, command }) => {
+	let ppown = 'https://telegra.ph/file/f124c0ce3b165014f02ee.jpg'
+let teksbio = `
 
-📮 *Note:*
-• Owner tidak menerima save contact
-• Owner berhak blockir tanpa alasan
-• Berbicaralah yang sopan & tidak spam
-• Owner Hanya merespon yang berkaitan dengan BOT
-• No Telp`
-
-//------------ BIO
-let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
-let teksbio = `${htki} *BIODATA* ${htka}
 *ɴᴀᴍᴇ:* GopalAsu 
 *ᴀɢᴇ:* 19ᵗʰ
 *sᴛᴀᴛᴜs:* single 
 
 *ʙɪʀᴛʜᴅᴀʏ:* 27 Jan 2003
-*ᴀᴅᴅʀᴇss:* Indonesia, Jawa Timur, Surabaya
+*ᴀᴅᴅʀᴇss:* Indonesia, Jawa Timur, Surabaya`
+let foot = `
+📷 *Instagram:*
+   ╰► ${sig}
+🐈 *Github:* 
+  ╰► ${sgh}
+🥏 *Whatsapp*
+  ╰► wa.me/${nomorown}`
 
-📷 *Instagram:* ${sig}
-🐈 *Github:* ${sgh}
-🥏 *Whatsapp* wa.me/${nomorown}
-
-`
-  let teks = ' '
-const sections = [
-   {
-	title: `${htjava} OWNER –––––––––·•`,
-	rows: [
-	    {title: "📱 • Nomor", rowId: ".owner nomor"},
-	{title: "🎨 • Biodata", rowId: ".owner bio"},
-	{title: "🌎 • Script", rowId: ".sc"},
-	]
-    },{
-	title: `${htjava} SUPPORT ME –––––––·•`,
-	rows: [
-	    {title: "💹 • Donasi", rowId: ".owner nomor"},
-	{title: "🔖 • Sewa", rowId: ".sewa"},
-	{title: "🌟 • Buy Premium", rowId: ".premium"},
-	]
-  },
-]
-
-const listMessage = {
-  text: teks,
-  footer: null,
-  title: `${htki} *OWNER* ${htka}`,
-  buttonText: "Click Here !",
-  sections
-}
-
-  try {
-    if (/(creator|owner)/i.test(command)) {
-      const count = args[1] && args[1].length > 0 ? Math.min(99999999, Math.max(parseInt(args[1]), 1)) : !args[1] || args.length < 3 ? 1 : Math.min(1, count)
-        switch (type) {
-          case 'nomor':
-          conn.reply(m.chat, teksnomor, m, { contextInfo: { mentionedJid: [nowner] }})
-            break
-            case 'bio':
-          conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorown, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
-            break
-            
-          default:
-            return await conn.sendMessage(m.chat, listMessage, { quoted: m, contextInfo: { mentionedJid: [m.sender] }})
-        }
-    } else if (/aoaooaoaooaoa/i.test(command)) {
-      const count = args[2] && args[2].length > 0 ? Math.min(99999999, Math.max(parseInt(args[2]), 1)) : !args[2] || args.length < 4 ? 1 :Math.min(1, count)
-      switch (_type) {
-        case 't':
-          break
-        case '':
-          break
-
-        default:
-          return conn.sendButton( m.chat, caption, wm, null, [`⋮☰ Menu`, `.menu`], m)
-      }
+const fload = {
+    key : {
+    remoteJid: 'status@broadcast',
+    participant : '0@s.whatsapp.net'
+    },
+    message: {
+    orderMessage: {
+    itemCount : 666,
+    status: 1,
+    surface : 1,
+    message: 'Nih kak Ownerku yang ganteng',
+    orderTitle: `▮Menu ▸`,
+    thumbnail: await(await fetch(ppown)).buffer, 
+    sellerJid: '0@s.whatsapp.net' 
     }
-  } catch (err) {
-    m.reply("Error\n\n\n" + err.stack)
-  }
+    }
+    }
+    await conn.sendButton(m.chat, teksbio, foot, ppown, [['ᴅᴏɴᴀsɪ', '.donasi']], fload)
+
+
 }
-
-handler.help = ['owner', 'creaor']
-handler.tags = ['main', 'info']
-handler.command = /^(owner|creator)/i
-
+handler.help = ['owner']
+handler.tags = ['info']
+handler.command = ['creator', 'owner']
 export default handler
